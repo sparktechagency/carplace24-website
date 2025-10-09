08a7196330560216a6c266ea9fe1bec38ca518b2
Mahmud Hasan Sabbir <mahmoodsabbir3087@gmail.com>
2025-10-09 18:39:26 +0600

    feat(website): add basic pages for blogs, about, vehicles and contact
    Add initial page components for website sections to establish basic routing structure

---
+++ src/app/(website)/blogs/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>Blog Page</div>;
+};
+
+export default page;
---
+++ src/app/(website)/vehicles/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>Vehicles Page</div>;
+};
+
+export default page;
